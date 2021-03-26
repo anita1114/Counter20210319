2db89e11c8ceb7326b7ee1d65162f63b5bee27d3
student <s1081650@gm.pu.edu.tw>
2021-03-26 16:03:07 +0800

    OnClickListener

--- app/src/main/java/tw/edu/pu/csim/tcyang/counter20210319/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/counter20210319/MainActivity.kt
@@ -5,44 +5,62 @@ import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(),View.OnClickListener {
+class MainActivity : AppCompatActivity(),
+    View.OnClickListener,View.OnLongClickListener {
 
-    var counter:Int = 0
+        var counter: Int = 0
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        txv.text = counter.toString()
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+            txv.text = counter.toString()
 
-        txv.setOnClickListener(this)
-        btn.setOnClickListener(this)
-        btn2.setOnClickListener(this)
-        btn3.setOnClickListener(this)
-    }
+            txv.setOnClickListener(this)
+            btn.setOnClickListener(this)
+            btn2.setOnClickListener(this)
+            btn3.setOnClickListener(this)
+            btnhappy.setOnClickListener(object : View.OnClickListener {
+                override fun onClick(v: View?) {
+                    counter = (1..100).random()
+                    txv.text = counter.toString()
+                }
+            })
 
-
-    fun Add(v:View){
-        //if (v.id == R.id.btn){
-        if (v ==  btn){
-            counter++
+            img.setOnLongClickListener(this)
         }
-        else{
-            counter += 2
-        }
-        txv.text = counter.toString()
-    }
 
-    override fun onClick(v: View?) {
-        if(v==txv || v==btn){
-            counter++
+
+        fun Add(v: View) {
+            //if (v.id == R.id.btn){
+            if (v == btn) {
+                counter++
+            } else {
+                counter += 2
+            }
+            txv.text = counter.toString()
         }
-        else if(v==btn2){
+
+        override fun onClick(v: View?) {
+            if (v == txv || v == btn) {
+                counter++
+            } else if (v == btn2) {
+                counter += 2
+            } else {
+                counter = 0
+            }
+            txv.text = counter.toString()
+        }
+
+    override fun onLongClick(v: View?): Boolean {
+        if(v==txv){
             counter+=2
         }
-        else {
-            counter = 0
+        else{
+            counter--
         }
+        counter+=2
         txv.text=counter.toString()
+        return true
     }
 
 
